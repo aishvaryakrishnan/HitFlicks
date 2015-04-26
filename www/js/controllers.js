9a8aac412126cb4276eec84bed2d15fb7ed15e40
Aishvarya Krishnan <aishvarya.krishnan@gmail.com>
2015-04-26 17:17:22 -0400

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -39,6 +39,14 @@ angular.module('starter.controllers', [])
     }
   };
 })
+.directive("wordcloud", function($window) {
+  return{
+    restrict: "EA",
+    template: "<svg width='850' height='200'></svg>",
+    link: function(scope, elem, attrs){
+    }
+  };
+})
 
 .controller('DashCtrl', function($scope) {
 
@@ -641,8 +649,68 @@ legend.append("text")
         });
         
         $scope.getMovie = function(movie) {
-        alert(movie.Title);	
+        
+        var fill = d3.scale.category20();
+  var data = movie.cnt;
+
+d3.layout.cloud().size([500, 500])
+      .words(data.map(function(d) {
+        return {text: d.word, size: d.freq};
+      }))
+      .padding(5)
+      .rotate(function() { return ~~(Math.random() * 2) * 90; })
+      .font("Impact")
+      .fontSize(function(d) { return d.size; })
+      .on("end", draw)
+      .start();
+
+  function draw(words) {
+      d3.select("wordcloud").append("svg")
+        .attr("width", 300)
+        .attr("height", 300)
+      .append("g")
+        .attr("transform", "translate(150,150)")
+      .selectAll("text")
+        .data(words)
+      .enter().append("text")
+        .style("font-size", function(d) { return d.size + "px"; })
+        .style("font-family", "Impact")
+        .style("fill", function(d, i) { return fill(i); })
+        .attr("text-anchor", "middle")
+        .attr("transform", function(d) {
+
+          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function(d) { return d.text; });
+  }
+  
         };
+        function drawUpdate(words){
+   d3.layout.cloud().size([500, 500])
+      .words(words)
+      .padding(5)
+      .rotate(function() { return ~~(Math.random() * 2) * 90; })
+      .font("Impact")
+      .fontSize(function(d) { return d.size; })
+      .start();
+
+
+      d3.select("svg")
+      .selectAll("g")
+        .attr("transform", "translate(150,150)")
+      .selectAll("text")
+        .data(words).enter().append("text")
+        .style("font-size", function(d) { return d.size + "px"; })
+        .style("font-family", "Impact")
+        .style("fill", function(d, i) { return fill(i); })
+
+        .attr("transform", function(d) {
+
+          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function(d) { return d.text; });
+
+  }
   // Inspired by http://informationandvisualization.de/blog/box-plot
 d3.box = function() {
   var width = 1,

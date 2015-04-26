9fa89753c876fa24d91c8fccf5d09fa36c7d766b
Aishvarya Krishnan <aishvarya.krishnan@gmail.com>
2015-04-26 17:54:36 -0400

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -649,40 +649,39 @@ legend.append("text")
         
         $scope.getMovie = function(movie) {
         $scope.showChart = true;
-        var fill = d3.scale.category20();
-  var data = movie.cnt;
+        var frequency_list = movie.cnt;
 
-d3.layout.cloud().size([850, 700])
-      .words(data.map(function(d) {
-        return {text: d.word, size: d.freq};
-      }))
-      .padding(5)
-      .rotate(function() { return ~~(Math.random() * 2) * 90; })
-      .font("Impact")
-      .fontSize(function(d) { return d.size; })
-      .on("end", draw)
-      .start();
 
-  function draw(words) {
-      d3.select("wordcloud").append("svg")
-        .attr("width", 500)
-        .attr("height", 500)
-      .append("g")
-        .attr("transform", "translate(150,150)")
-      .selectAll("text")
-        .data(words)
-      .enter().append("text")
-        .style("font-size", function(d) { return d.size + "px"; })
-        .style("font-family", "Impact")
-        .style("fill", function(d, i) { return fill(i); })
-        .attr("text-anchor", "middle")
-        .attr("transform", function(d) {
+    var color = d3.scale.linear()
+            .domain([0,1,2,3,4,5,6,10,15,20,100])
+            .range(["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", "#555", "#444", "#333", "#222"]);
 
-          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-        })
-        .text(function(d) { return d.text; });
-  }
-  
+    d3.layout.cloud().size([800, 300])
+            .words(frequency_list)
+            .rotate(0)
+            .fontSize(function(d) { return d.Freq; })
+            .on("end", draw)
+            .start();
+
+    function draw(words) {
+        d3.select("body").append("svg")
+                .attr("width", 850)
+                .attr("height", 350)
+                .attr("class", "wordcloud")
+                .append("g")
+                // without the transform, words words would get cutoff to the left and top, they would
+                // appear outside of the SVG area
+                .attr("transform", "translate(320,200)")
+                .selectAll("text")
+                .data(words)
+                .enter().append("text")
+                .style("font-size", function(d) { return d.Freq + "px"; })
+                .style("fill", function(d, i) { return color(i); })
+                .attr("transform", function(d) {
+                    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                })
+                .text(function(d) { return d.word; });
+    }
         };
         
   // Inspired by http://informationandvisualization.de/blog/box-plot
